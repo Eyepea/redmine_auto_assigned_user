cedf62fb6d315a5137a311a745a9f0cb224e5f6a
Ludovic Gasc <gmludo@gmail.com>
2010-10-25 11:39:04 +0200

    Create plugin structure with redmine_plugin script

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_auto_assigned_user do
+  name 'Redmine Auto Assigned User plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
