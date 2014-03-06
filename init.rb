570a409f96289825993e9e9eba2cb044560cfe7a
Ludovic Gasc <gmludo@gmail.com>
2014-03-06 14:22:31 +0100

    Update URL + new version number

--- init.rb
+++ init.rb
@@ -6,10 +6,10 @@ Redmine::Plugin.register :redmine_auto_assigned_user do
   name 'Redmine Auto Assigned User plugin'
   author 'Ludovic Gasc'
   description 'If an user forget to assign the issue, this plugin will auto-assign to the project manager.'
-  version '0.0.1'
-  url 'http://github.com/GMLudo/redmine_auto_assigned_user'
+  version '0.2.0'
+  url 'http://github.com/Eyepea/redmine_auto_assigned_user'
   author_url 'http://be.linkedin.com/in/ludovicgasc'
-  requires_redmine :version_or_higher => '0.9.0'
+  requires_redmine :version_or_higher => '2.0.0'
 
   settings :default => {
     'project_manager_role' => '',
